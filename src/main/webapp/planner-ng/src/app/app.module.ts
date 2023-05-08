431b2b6b9beae5d4ebaa4bceadf346853931c881
Roberto Angelucci <roberto.angelucci@gmail.com>
2023-05-08 08:52:28 +0200

    Capabilities added: Login form and security component

--- src/main/webapp/planner-ng/src/app/app.module.ts
+++ src/main/webapp/planner-ng/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {ReactiveFormsModule} from "@angular/forms";
+import { LoginFormComponent } from './login-form/login-form.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {GuardInterceptor} from "./helper/guard.interceptor";
+import { DashboardFormComponent } from './dashboard-form/dashboard-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    DashboardFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: GuardInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
