b494886aed4fb2c4f17633623624034be00ac5ae
RiccardoBagaglini <riccardo.bagaglini@tech.iagica.it>
2023-05-09 16:51:13 +0200

    traduzioneRiccardo

--- src/main/webapp/planner-ng/src/app/app.component.ts
+++ src/main/webapp/planner-ng/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './services/auth/auth.service';
 import { TranslateService } from '@ngx-translate/core';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +10,24 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent implements OnInit {
   title = 'planner-ui';
   userEmail: any;
+  defaultLenguage: any;
+  arrayLenguage: Array<any> = [
+    { key: 'English', value: 'en' },
+    { key: 'Italiano', value: 'it' },
+    { key: 'Espanol', value: 'es' },
+  ];
 
   constructor(
     private authService: AuthService,
     private translate: TranslateService
   ) {
-    translate.setDefaultLang('it');
-    translate.use('it');
+    translate.getBrowserLang();
+    this.defaultLenguage = translate.getBrowserLang();
+
+    if (!!this.defaultLenguage) {
+      translate.setDefaultLang(this.defaultLenguage);
+      translate.use(translate.defaultLang);
+    }
   }
 
   ngOnInit(): void {}
@@ -24,9 +36,11 @@ export class AppComponent implements OnInit {
     return this.authService.isLogged();
   }
 
-  useLanguage(language: any): void {
-    this.translate.use(language);
-    //console.log(this.translate.getBrowserLang());
-    //console.log(this.translateService.instant('home.getEvents', {name: 'John'}));
+  select(value: any) {
+    let actualLenguage = value.target.value;
+    this.translate.setDefaultLang(actualLenguage);
+    this.translate.use(actualLenguage);
+    //Per settare la traduzione instantanee bisogna poter inserire, 
+    this.arrayLenguage.map(x => this.translate.instant(x.key));
   }
 }
